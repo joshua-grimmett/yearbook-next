688d08baa45733232969d819597a124bbe2c92c0
Joshua <joshua@grimmett.com.au>
2022-10-15 22:46:12 +1000

    Images

---
+++ public/classes/PS/data.js
@@ -0,0 +1,67 @@
+module.exports = {
+  title: 'PREP S',
+  teachers: [
+      {name: 'Rachel Shepherd', image: '/staff/Shepherd, Rachel.jpg'},
+      {name: 'Brooke Dawson', image: '/staff/Dawson, Brooke.png'},
+  ],
+  poster: '/classes/PS/poster.png',
+  pageNumbers: [40, 41],
+  colours: 'prep',
+  tableTextStyle: {
+    fontSize: '8.5pt',
+    lineHeight: '12pt'
+  },
+  classPhotos: [
+      { image: '/classes/PS/images/0.JPG', caption: 'Mila and Harper love using their iPads.' },
+      { image: '/classes/PS/images/3.JPG', caption: 'Play dough is the best according to Jax and Lennon.' },
+      { image: '/classes/PS/images/6.JPG', caption: 'Ellodi, Isla and Emily enjoying the Fairy Tale Castle.', top: '-4mm' },
+      { image: '/classes/PS/images/7.JPG', caption: 'Charlotte and Oliver relaxing in the castle.', top: '-7mm' },
+      { image: '/classes/PS/images/1.JPG', caption: 'Jeremy and Kyrie-Cruz fishing for Red Words.' },
+      { image: '/classes/PS/images/2.JPG', caption: 'Laura and Zoran writing their Red Words.' },
+      { image: '/classes/PS/images/4.JPG', caption: 'Leo and Ante using play dough to strengthen their fingers.' },
+      { image: '/classes/PS/images/5.JPG', caption: 'Chloe and Gabriella enjoying fine motor activities.' },
+      { image: '/classes/PS/images/8.JPG', caption: 'We have had fun learning with our friends this year – Isaac, Noah and Arthur.', top: '-10mm' },
+      { image: '/classes/PS/images/13.JPG', caption: 'Ante, Jax and Lennon loving lunch time.', top: '-5mm' },
+      { image: '/classes/PS/images/10.JPG', caption: 'Harleigh and Coleton hanging around.', top: '-8mm' },
+      { image: '/classes/PS/images/11.JPG', caption: 'Sea World was the BEST!' },
+      { image: '/classes/PS/images/12.JPG', caption: 'Arthur, Charlotte, Laura and Noah enjoying the sandpit.' },
+      { image: '/classes/PS/images/9.JPG', caption: 'Is that Spider Man or is it Jesse?', top: '-7mm' },
+      { image: '/classes/PS/images/14.JPG', caption: 'Smiles all round – Chloe, Gabriella, Isla and Harper.' },
+      { image: '/classes/PS/images/15.JPG', caption: 'Leo, Noah, Oliver and Jeremy having fun in the sun.' },
+  ],
+  tableText: [
+      {name: 'Kyrei-Cruz Ahipene', job: 'A policeman'},
+      {name: 'Jeremy Beijeman', job: 'A worker'},
+      {name: 'Oliver Carr', job: 'A superhero'},
+      {name: 'Arthur Cho', job: 'A batman ninja'},
+      {name: 'Laura Craig', job: 'A teacher'},
+      {name: 'Zoran Dimoski', job: 'A baker'},
+      {name: 'Emily Doolan', job: 'A vet'},
+      {name: 'Mila Farrell', job: 'A police lady'},
+      {name: 'Ante Gavet', job: 'An astronaut'},
+      {name: 'Leo Green', job: 'A policeman'},
+      {name: 'Isla Heeb-Waters', job: 'A doctor'},
+      {name: 'Charlotte Huthwaite', job: 'A teacher'},
+      {name: 'Jax Lord', job: 'A fire fighter'},
+      {name: 'Lennon Marcic', job: 'A worker with my dad'},
+      {name: 'Isaac Parsonson', job: 'A scientist'},
+      {name: 'Ellodi Penaud', job: 'A police lady'},
+      {name: 'Harper Preece', job: 'A fire fighter'},
+      {name: 'Harleigh Russell', job: 'Work at McDonalds'},
+      {name: 'Chloe Scott', job: 'A police lady'},
+      {name: 'Jesse Thorburn', job: 'An actor'},
+      {name: 'Coleton To', job: 'A fire fighter'},
+      {name: 'Gabriella Walker', job: 'A police lady'},
+      {name: 'Noah Walsh', job: 'A builder'}
+  ],
+  text: `This year has been the first time in a few years that we have spent all year together at school (give and take a bit of sickness along the way). Looking back to the beginning of the new year when our preppies were shy and nervous, it is always so rewarding to see how far they all have come, both in confidence and independence, but also socially and emotionally.
+  <br/>
+  <span class="break"></span>
+  We asked our students, and they nominated the following as highlights of
+  their year:<br> 
+  <div class="centerText">• Meeting our new classmates and teachers • The Easter Concert and Easter Bunny visit • Sea World • Sports Days • Mother’s/Fairy Day • Under 8’s Day • Father’s/Pirates Day • Learning about Sea Animals • Maths • Learning • Playing • Book Week • Pyjama Day</span>
+  <span class="break"></div>
+  We are especially grateful to have each and every member of Prep S in our
+  class this year, and we can’t wait to see you all continue to grow and develop
+  over the next few years.`
+}
