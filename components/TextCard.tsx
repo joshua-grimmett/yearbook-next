8ec91da49124c4950fa7cee31978678d2ee1a11e
Joshua <joshua@grimmett.com.au>
2022-10-16 16:36:31 +1000

    Add large variant

--- components/TextCard.tsx
+++ components/TextCard.tsx
@@ -1,7 +1,7 @@
 
-const TextCard = ({ text, textStyle }: { text: string, textStyle: object }) => {
+const TextCard = ({ text, textStyle, large }: { text: string, textStyle: object, large: boolean }) => {
   return (
-    <div className="text__card" style={textStyle}>
+    <div className={`text__card${large ? ' text__card--large': ''}`} style={textStyle}>
       <span dangerouslySetInnerHTML={{__html: text}}></span>
     </div>
           
