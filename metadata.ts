ded211b6635c7481c870362e9a12773302ddcfc7
Joshua <joshua@grimmett.com.au>
2022-10-16 16:36:06 +1000

    ts

--- metadata.ts
+++ metadata.ts
@@ -6,14 +6,14 @@ import _5LN from './public/classes/5LN/data'
 import _5W from './public/classes/5W/data'
 import _6WC from './public/classes/6WC/data'
 
-const classes = [
+const classes: any[] = [
   // PS,
-  _3B,
+  // _3B,
   // _3M,
   // _4W,
   // _5LN,
   // _5W,
-  // _6WC,
+  _6WC,
 ]
 
 export { classes }
