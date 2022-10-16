7f065d1815012be867f3dad07f2dc63a0477f76b
Joshua <joshua@grimmett.com.au>
2022-10-16 14:52:36 +1000

    Use metadata for testing

---
+++ metadata.js
@@ -0,0 +1,19 @@
+import PS from './public/classes/PS/data'
+import _3B from './public/classes/3B/data'
+import _3M from './public/classes/3M/data'
+import _4W from './public/classes/4W/data'
+import _5LN from './public/classes/5LN/data'
+import _5W from './public/classes/5W/data'
+import _6WC from './public/classes/6WC/data'
+
+const classes = [
+  // PS,
+  _3B,
+  // _3M,
+  // _4W,
+  // _5LN,
+  // _5W,
+  // _6WC,
+]
+
+export { classes }
