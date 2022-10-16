b7837c08e19528fb5c37e766799a06b286764eb8
Joshua <joshua@grimmett.com.au>
2022-10-16 16:36:26 +1000

    Initialise quad pages

--- components/Page.tsx
+++ components/Page.tsx
@@ -3,19 +3,45 @@ import Footer from './Footer';
 
 import type { ClassPhoto } from './ClassPhotoCards';
 import type { Teacher } from './TeacherCard';
-import { ClassLeftGrid, ClassRightGrid } from './ClassGrids';
+import { ClassLeftGrid, ClassRightGrid, LargeClassLeftGrid } from './ClassGrids';
 
 const DoublePage = (props: any) => {
-  console.log(props.className)
   return (
     <div className={`page ${props.className}`}>
-      <LeftPage pageNumber={props.pageNumbers[0]} {...props}/>
-      <RightPage pageNumber={props.pageNumbers[1]} {...props}/>
+      <NormalLeftPage pageNumber={props.pageNumbers[0]} {...props}/>
+      <NormalRightPage pageNumber={props.pageNumbers[1]} {...props}/>
     </div>
   )
 }
 
-const LeftPage = ({
+export default DoublePage
+
+
+const LargeDoublePage = (props: any) => {
+  return (
+    <div className={`page ${props.className}`}>
+      <LargeLeftPage pageNumber={props.pageNumbers[0]} {...props}/>
+      <NormalLeftPage pageNumber={props.pageNumbers[1]} {...props}/>
+    </div>
+  )
+}
+
+export { LargeDoublePage }
+
+
+const PhotoDoublePage = (props: any) => {
+  return (
+    <div className={`page ${props.className}`}>
+      <LargeLeftPage pageNumber={props.pageNumbers[2]} {...props}/>
+      <LargeRightPage pageNumber={props.pageNumbers[3]} {...props}/>
+    </div>
+  )
+}
+
+export { PhotoDoublePage }
+
+
+const NormalLeftPage = ({
   title, pageNumber, teachers, text, classPhotos, textStyle
 }: { 
   title: string,
@@ -36,7 +62,8 @@ const LeftPage = ({
   )
 }
 
-const RightPage = ({
+
+const NormalRightPage = ({
   pageNumber,
   classPhotos,
   tableText,
@@ -69,6 +96,60 @@ const RightPage = ({
   )
 }
 
-export default DoublePage
-
 export type Direction = 'left' | 'right';
+
+const LargeLeftPage = ({
+  title, pageNumber, teachers, text, classPhotos, textStyle
+}: {
+  title: string,
+  pageNumber: number,
+  teachers: Teacher[],
+  text: string,
+  classPhotos: ClassPhoto[],
+  textStyle: object
+}) => {
+  return (
+    <div className="left">
+      <div className="page__content">
+        <Header direction="left" title={title}/>
+        <LargeClassLeftGrid teachers={teachers} text={text} classPhotos={classPhotos} textStyle={textStyle}/>
+      </div>
+      <Footer direction="left" pageNumber={pageNumber}/>
+    </div>
+  )
+}
+
+export { LargeLeftPage }
+
+const LargeRightPage = ({
+  pageNumber,
+  classPhotos,
+  tableText,
+  question,
+  poster,
+  teachers,
+  tableTextStyle
+}: {
+  title: string,
+  pageNumber: number,
+  classPhotos: ClassPhoto[],
+  tableText: string,
+  question: string,
+  poster: string,
+  teachers: Teacher[],
+  tableTextStyle: object
+}) => {
+  return (
+    <div className="right">
+      <div className="page__content">
+        <ClassRightGrid
+          question={question}
+          text={tableText}
+          tableTextStyle={tableTextStyle}
+          classPhotos={classPhotos.slice(teachers.length > 1 ? 7 : 8)}
+          poster={poster}/>
+      </div>
+      <Footer direction="right" pageNumber={pageNumber}/>
+    </div>
+  )
+}
