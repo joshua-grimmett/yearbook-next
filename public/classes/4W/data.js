d2edf5f410b6569af5e7b66b55498a1613643c46
Joshua <joshua@grimmett.com.au>
2022-10-17 12:00:26 +1000

    Fixed photo

--- public/classes/4W/data.js
+++ public/classes/4W/data.js
@@ -15,7 +15,7 @@ module.exports = {
     {image: '/classes/4W/images/6.jpg', caption: 'Free dress fun'},
     {image: '/classes/4W/images/7.jpg', caption: 'Cheeky smiles'},
     {image: '/classes/4W/images/8.jpg', caption: 'Go Colman!'},
-    {image: '/classes/4W/images/16.jpg', caption: 'All smiles', top: '-1mm'},
+    {image: '/classes/4W/images/16.jpg', caption: 'All smiles', top: '-16mm'},
     {image: '/classes/4W/images/10.jpg', caption: 'Fun in the sun'},
     {image: '/classes/4W/images/11.jpg', caption: 'Siganto smiles'},
     {image: '/classes/4W/images/12.jpg', caption: 'Hanging with mates', top: '-6mm'},
