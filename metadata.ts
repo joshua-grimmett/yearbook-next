d71370664a36a629da4d8486769c488e1a8bb02b
Joshua <joshua@grimmett.com.au>
2022-10-22 20:26:46 +1000

    4KP

--- metadata.ts
+++ metadata.ts
@@ -22,6 +22,7 @@ import _34C from './public/classes/34C/data'
 import _4W from './public/classes/4W/data'
 import _4B from './public/classes/4B/data'
 import _4E from './public/classes/4E/data'
+import _4KP from './public/classes/4KP/data'
 import _5LN from './public/classes/5LN/data'
 import _5W from './public/classes/5W/data'
 import _5C from './public/classes/5C/data'
@@ -54,7 +55,8 @@ const classes: any[] = [
   // _34C,
   // _4W,
   // _4B,
-  _4E,
+  // _4E,
+  _4KP,
   // _5LN,
   // _5W,
   // _5C,
---
+++ public/classes/4KP/data.js
@@ -0,0 +1,119 @@
+module.exports = {
+  title: '4KP',
+  teachers: [
+    {name: 'Karen Caswell', image: '/staff/Caswell, Karen.jpg'},
+    {name: 'Mia Pinnington', image: '/staff/Pinnington, Mia.jpg'},
+  ],
+  posters: ['/classes/3WH/poster.png', '/classes/3WH/poster.png'],
+  pageNumbers: [0,0],
+  type: 'xl',
+  colours: 'yr3',
+  classPhotos: [
+    {image: '/classes/4KP/images/01.jpg', caption:  'Relaxing with reading!', top: '-5mm'},
+    {image: '/classes/4KP/images/04.jpg', caption:  'Camp Fun – Games Night'},
+    {image: '/classes/4KP/images/03.jpg', caption:  'Fractions Fun'},
+    {image: '/classes/4KP/images/09.jpg', caption:  'HASS Dress-up Day'},
+    {image: '/classes/4KP/images/08.jpg', caption:  'Games Night Fun'},
+    {image: '/classes/4KP/images/02.jpg', caption:  'Learning in HASS', top: '-7mm'},
+    {image: '/classes/4KP/images/24.jpg', caption: 'Wacky Free Dress Day', top: '-6mm'},
+    {image: '/classes/4KP/images/05.jpg', caption:  'Ropes Challenge - Camp', top: '-11mm'},
+    {image: '/classes/4KP/images/06.jpg', caption:  'Sports Fun Day – Go Pedley', top: '-10mm'},
+    {image: '/classes/4KP/images/10.jpg', caption: 'Camp Buddies'},
+    {image: '/classes/4KP/images/11.jpg', caption: 'Achieving Character Goals'},
+    {image: '/classes/4KP/images/12.jpg', caption: 'Sports Fun Day – Go Foxwell'},
+    {image: '/classes/4KP/images/13.jpg', caption: 'Sports Fun Day – Go Colman'},
+    {image: '/classes/4KP/images/14.jpg', caption: 'Let’s Learn'},
+    {image: '/classes/4KP/images/23.jpg', caption: 'Working Hard'},
+    {image: '/classes/4KP/images/16.jpg', caption: 'Camp Games Night'},
+    {image: '/classes/4KP/images/17.jpg', caption: 'Bodyboarding Buddies'},
+    {image: '/classes/4KP/images/18.jpg', caption: 'Awesome Foursome'},
+    {image: '/classes/4KP/images/19.jpg', caption: 'We love reading!'},
+    {image: '/classes/4KP/images/20.jpg', caption: 'Great Helpers'},
+    {image: '/classes/4KP/images/21.jpg', caption: 'We are Wacky', top: '-9mm'},
+    {image: '/classes/4KP/images/22.jpg', caption: 'The beach is the place to be!', top: '-13mm'},
+    {image: '/classes/4KP/images/25.jpg', caption: 'Fabulous Four!', top: '-9mm'},
+    {image: '/classes/4KP/images/07.jpg', caption:  'Kindness Crew Art', top: '-5mm'},
+    {image: '/classes/4KP/images/15.jpg', caption: 'Friends'},
+    {image: '/classes/4KP/images/26.jpg', caption: 'Working together'},
+    {image: '/classes/4KP/images/27.jpg', caption: 'We like free dress!'},
+    {image: '/classes/4KP/images/28.jpg', caption: 'Collaborating with Classmates'},
+    {image: '/classes/4KP/images/29.jpg', caption: 'Sports Fun Day winners'},
+    {image: '/classes/4KP/images/30.jpg', caption: 'Group Brainstorm'},
+    {image: '/classes/4KP/images/31.jpg', caption: 'Team Work Makes the Dream Work'},
+    {image: '/classes/4KP/images/32.jpg', caption: 'Ropes Challenge Support Team '},
+  ],
+  tableText: [
+    {name: 'Aries	Ahipene', job: 'Singing with my friends and teachers on the bus on the way to swimming. '},
+    {name: 'Alex	Watson', job: 'That I loved learning all the new things.'},
+    {name: 'Bailey	Bowen', job: 'When I did a good job on my work. My teachers would give me feedback that made me proud. '},
+    {name: 'Baylin	Braga', job: 'Opening shops to learn about money in maths.'},
+    {name: 'Brucie	Fatetolu', job: 'The fun activities we did to learn.'},
+    {name: 'Charlie	Needham', job: 'That I loved playing with my friends at lunch.'},
+    {name: 'Charlotte	Paki', job: 'Everything! It was the best year ever.'},
+    {name: 'Chhaiya	Leng', job: 'Making new friends. '},
+    {name: 'Colton	Anning', job: 'That I loved when we did a treasure hunt in maths all over the school. '},
+    {name: 'Connor	Sinclair', job: 'Earning diamond and all the fun lessons we do. '},
+    {name: 'Dallas	Liddington', job: 'Getting to learn with my friends. '},
+    {name: 'Ella	Bourke', job: 'When we got to do learning on Minecraft. '},
+    {name: 'Emily 	Dearlove', job: 'When we had class picnics in the sun at lunch, listening to music with our teachers. '},
+    {name: 'Emmett	Smith', job: 'When we wrote persuasives to convince our teachers for a fun day. It was the best day ever. '},
+    {name: 'Evie	Brettle', job: 'That I loved sharing my ideas in my writing. '},
+    {name: 'Evie 	Liukuey', job: 'Learning new stuff in fun ways. '},
+    {name: 'Gracie	Heeb-Waters', job: 'Learning with Minecraft.'},
+    {name: 'Gwen	Stockdale', job: 'Lessons when we got to move around like playground hunts.'},
+    {name: 'Haylo	Banfield', job: 'Kahoot quizzes. '},
+    {name: 'Hendrix	Palupe', job: 'That I loved brainstorming all my ideas. '},
+    {name: 'Honey	Tahau', job: 'Swimming and when I got to draw. '},
+    {name: 'Ieshah	Campbell', job: 'Making new friends and my amazing teachers. '},
+    {name: 'Indie	Clark', job: 'That I loved doing all the experiments in science. '},
+    {name: 'Kaitiaki	Tonga-Awhikau', job: 'That I loved the fun maths lessons.'},
+    {name: 'Kendall	Tunnicliffe', job: 'Doing science experiments and also learning about space.'},
+    {name: 'Knoxyn	Waaka', job: 'That I loved all of it. Everyday was great. '},
+    {name: 'Lara	Hollis', job: 'Fun experiments in science and when we did arts or crafts.'},
+    {name: 'Leara	Fletcher', job: 'Fun math lessons.'},
+    {name: 'Lili	Yu', job: 'Learning maths. I loved when it was tricky. '},
+    {name: 'Madison	Goddard', job: 'Making new friends.'},
+    {name: 'Maliyaan	Compton', job: 'PE, Science and when I won the cubby competition and got to paint the SWAG cubby with my dad. '},
+    {name: 'Mason	Jackson', job: 'When we did fun lessons like Minecraft in maths or playground hunts.'},
+    {name: 'Meggie	Ferrier', job: 'Meeting Miss. Wolbers and Mrs. Hardwick. They are the best. Their lessons were so fun. '},
+    {name: 'Mia	Bodley', job: 'Minecraft maths lessons. '},
+    {name: 'Millie	Playforth', job: 'All the fun lessons we did.'},
+    {name: 'Mischa	Young', job: 'My teachers because they are the best and make school too fun. '},
+    {name: 'Muhammed	Ozturk', job: 'Getting diamond and also when my brother got to join our class for a few days. '},
+    {name: 'Nixon	Campbell', job: 'Science experiments were the best. '},
+    {name: 'Ricky	David', job: 'That I loved doing cool experiments in science and then learning about space.'},
+    {name: 'Sara	Schilling', job: 'Doing fun lessons. I really liked learning new things this year. '},
+    {name: 'Savanah	Constant', job: 'Music lessons. '},
+    {name: 'Sienna	Coe', job: 'Science because we did experiments and fun stuff all the time. '},
+    {name: 'Sienna	Van Der Stok', job: 'That my teachers were very nice. I loved when Mrs. Amies would read pig the pug books to us. '},
+    {name: 'Sofia	Miller', job: 'My teachers. They are fun and caring and that made me feel happy every day. '},
+    {name: 'Talarni	Smith', job: 'Learning about the solar system. '},
+    {name: 'Talia	Fatafehi', job: 'Spelling tests!'},
+    {name: 'Tiaro	Balla Zsolnsi', job: 'Playing soccer.'},
+    {name: 'Tyne	Danielson', job: 'Doing the work. My teachers made every lesson fun. I also loved when my mum and grandma came in to share for our history lesson. '},
+    {name: 'Zavier	Saab', job: 'Maths. I loved when Miss. Wolbers would give me challenges. '},
+  ],
+  tableTextStyle: {
+    lineHeight: '5pt',
+    fontSize: '5pt',
+  },
+  text: `We have had an awesome year in 3WH. We have made so many amazing memories and had some tremendous experiences. We have learnt so much in fun and exciting ways like playground hunts, cooking, constructing cities and opening a museum. We were so incredibly proud of all the hard work the children have put in this year.
+  <span class="break"></span>
+  We loved having our families come in for our HASS celebration day & global goals presentations and we would like to thank the parents who made a special effort to share their cultural celebrations to our class. Learning from people who have lived through it and experienced it is the best kind of lessons and the kids were inspired by you all. 
+  <span class="break"></span>
+  3WH loved using our exciting behaviour reward system of TeacherMon and we hope that students will keep these exclusive cards for many years to come. 
+  <span class="break"></span>
+  We have strived to provide challenging and supportive learning experiences for each and every student to help them reach their full potential. Our excursion was epic! 
+  <span class="break"></span>
+  We loved throwing boomerangs and creating beautiful traditional dot paintings and of course, dancing to the digeridoo. 
+  <span class="break"></span>
+  3WH had the best time creating our very own one day only museum in our classroom full of adorable creatures and amazing displays. 
+  <span class="break"></span>
+  We were very excited to improve our swimming skills in term 3. We loved sing alongs on the bus trips and couldn’t wait to test out our speed at the swimming carnival. 
+  <span class="break"></span>
+  We are so proud of how far you have come this year. 3WH have made new friends and developed our creativity, collaboration and character. We have reached many goals, mastered many skills, learnt many things and of course we have had a lot of fun and laughter along the way. We have absolutely loved being 3WH’s teachers and will miss them greatly next year.`,
+  textStyle: {
+    fontSize: '8.5pt',
+    lineHeight: '12pt'
+  }
+}
