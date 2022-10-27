dce28d029aab73ba0f40ac9e3b04d308f252b4f3
Joshua <joshua@grimmett.com.au>
2022-10-27 12:06:50 +1000

    6K

---
+++ public/classes/6K/data.js
@@ -0,0 +1,44 @@
+module.exports = {
+  title: '6K',
+  teachers: [
+    {name: 'Caitlyn King', image: '/staff/King, Caitlyn.jpg'},
+  ],
+  poster: '/classes/6K/poster.png',
+  pageNumbers: [0,0],
+  colours: 'yr6',
+  classPhotos: [
+    {image: '/classes/6K/images/img1.jpg', caption: 'Tilly & Amelia ', top: '-11mm'},
+    {image: '/classes/6K/images/img2.jpg', caption: 'Holly & Monica', top: '-12mm'},
+    {image: '/classes/6K/images/img3.jpg', caption: 'Jordan & Tyelan', top: '-4mm'},
+    {image: '/classes/6K/images/img4.jpg', caption: 'Hunter & Jacob', top: '-12mm'},
+    {image: '/classes/6K/images/img5.jpg', caption: 'Jordan & Tyelan (Awww)', top: '-21mm'},
+    {image: '/classes/6K/images/img6.jpg', caption: 'Alysha, Summer & Tilly ', top: '-11mm'},
+    {image: '/classes/6K/images/img7.jpg', caption: 'Noah, Kade, Henko & Hunter', top: '-13mm'},
+    {image: '/classes/6K/images/img8.jpg', caption: 'Sophia, Ella & Harrison', top: '-5mm'},
+    {image: '/classes/6K/images/img9.jpg', caption: 'Halle, Monica & Celina', top: '-11mm'},
+    {image: '/classes/6K/images/img22.jpg', caption:'Miss King, Monica & Holly ', top: '-3mm'},
+    {image: '/classes/6K/images/img12.jpg', caption:'Kade & Julian', top: '-27mm'},
+    {image: '/classes/6K/images/img16.jpg', caption:'Bodhi, Julian & Chris', top: '-11mm'},
+    {image: '/classes/6K/images/img13.jpg', caption:'Tilly & Montana'},
+    {image: '/classes/6K/images/img14.jpg', caption:'Malcolm & Balazs', top: '-11mm'},
+    {image: '/classes/6K/images/img15.jpg', caption:'Chase & Julian', top: '-8mm'},
+    {image: '/classes/6K/images/img11.jpg', caption:'Malcolm & Jacob', top: '-15mm'},
+    {image: '/classes/6K/images/img17.jpg', caption:'Monica & Tilly', top: '-12mm'},
+    {image: '/classes/6K/images/img18.jpg', caption:'Alysha, Holly & Celina'},
+    {image: '/classes/6K/images/img19.jpg', caption:'Hunter, Holly, Monica & Sophie', top: '-15mm'},
+    {image: '/classes/6K/images/img20.jpg', caption:'Holly & Montana', top: '-15mm'},
+    {image: '/classes/6K/images/img21.jpg', caption:'Ella & Sophie', top: '-12mm'},
+    {image: '/classes/6K/images/img10.jpg', caption:'Holly, Sophie & Montana', top: '-15mm'},
+  ],
+  text: `2022 has been an amazing year for all students in 6K! We started the year (a little late) as 26 individual students and finished the year as one big family. One of my favourite things about 6K this year has been their ability to work together and support one another no matter what. There isn’t one time where ‘groups’ or ‘cliques’ were present in our classroom. We are a class that prided ourselves on being kind, accepting and supportive and it made for a loving classroom environment. 
+  <span class="break"></span>
+  I will miss (your stories, your jokes, your hugs and your smiles, your laughter, your giggles, your out of the box thinking – your ideas – your voice reading out loud – your willingness to try new things – your face when you accomplish a goal – your kindness – your friendships – your imagination and creativity – your smiles – your banter – and even your attempts to claim Leo as your own) you!
+  <span class="break"></span>
+  Thank you for such a memorable year 6K. You guys will be a class to remember!
+  <span class="break"></span>
+  Love Miss King!`,
+  textStyle: {
+    fontSize: '8pt',
+    lineHeight: '10pt'
+  }
+}
