ff84b721372c9ed40a298e461e7ee698ce454c56
Joshua <joshua@grimmett.com.au>
2022-10-31 17:00:30 +1000

    2A, 2B

---
+++ public/classes/2A/data.js
@@ -0,0 +1,68 @@
+module.exports = {
+  title: '2A',
+  teachers: [
+      {name: 'Alanna Mackay', image: '/staff/Mackay, Alanna.jpg'},
+  ],
+  poster: '/classes/2A/poster.png',
+  pageNumbers: [40, 41],
+  colours: 'yr2',
+  classPhotos: [
+    {image: '/classes/2A/images/1.jpg', caption:  'Pyjama Day', top: '-14mm'},
+    {image: '/classes/2A/images/2.jpg', caption:  'Working collaboratively'},
+    {image: '/classes/2A/images/4.jpg', caption:  'Currumbin Wildlife Sanctuary'},
+    {image: '/classes/2A/images/3.jpg', caption:  'NAIDOC Week'},
+    {image: '/classes/2A/images/5.jpg', caption:  'Free dress day', top: '-6mm'},
+    {image: '/classes/2A/images/6.jpg', caption:  'TWOSday'},
+    {image: '/classes/2A/images/7.jpg', caption:  'Exciting experiments'},
+    {image: '/classes/2A/images/8.jpg', caption:  'Bullying No Way Day'},
+    {image: '/classes/2A/images/9.jpg', caption:  'Making slime', top: '-10mm'},
+    {image: '/classes/2A/images/13.jpg', caption: 'Footy and friends', top: '-6mm'},
+    {image: '/classes/2A/images/11.jpg', caption: 'Beenleigh Historical Village and Museum'},
+    {image: '/classes/2A/images/12.jpg', caption: 'The Old Coomera Lockup', top: '-19mm'},
+    {image: '/classes/2A/images/10.jpg', caption: 'Easter writing', top: '-15mm'},
+    {image: '/classes/2A/images/14.jpg', caption: 'Super Scientists', top: '-16mm'},
+    {image: '/classes/2A/images/15.jpg', caption: 'Buddy reading'},
+    {image: '/classes/2A/images/16.jpg', caption: 'Fun on the playground'},
+  ],
+  tableTextStyle: {
+    fontSize: '7.5pt',
+    lineHeight: '11pt'
+  },
+  question: 'When I grow up I want to be…',
+  tableText: [
+    {name: 'Elizabella	Andreou', job: 'a model'},
+    {name: 'Indiana	Beehag', job: 'a shop owner'},
+    {name: 'Molly	Blackaby', job: 'a doctor'},
+    {name: 'Arllo	Coleman', job: 'a rugby league player'},
+    {name: 'Elle-Avieve	Degroot', job: 'a gymnastics teacher'},
+    {name: 'James	Glen', job: 'a YouTuber'},
+    {name: 'Hone	Haimona', job: 'a YouTuber'},
+    {name: 'Qaasim	Hijazi', job: 'a pilot'},
+    {name: 'Kai	Inch', job: 'a YouTuber'},
+    {name: 'Benji	Khan', job: 'a YouTuber'},
+    {name: 'Jayden	Krause', job: 'a rugby league player'},
+    {name: 'Reid	Mason', job: 'a cosmetic surgeon'},
+    {name: 'Charlie	O’Shea', job: 'a soccer player'},
+    {name: 'Yasin	Ozturk', job: 'a police officer'},
+    {name: 'Lukka	Paterson', job: 'a boxer'},
+    {name: 'Zach	Perez', job: 'a mechanic'},
+    {name: 'Ashton	Royal', job: 'a plumber'},
+    {name: 'Piper	Sauer', job: 'a YouTuber'},
+    {name: 'River	Schmidt', job: 'a fashion designer'},
+    {name: 'Himani	Singh', job: 'a scientist'},
+    {name: 'Chloe	Stubbs', job: 'a ballerina'},
+    {name: 'Robin	Turrell', job: 'a soccer player'},
+    {name: 'Peyton	Van Der Westhuizen', job: 'a vet'},
+  ],
+  textStyle: {
+    fontSize: '8.5pt',
+    lineHeight: '11pt',
+  },
+  text: `Wow, what an incredible year 2022 has been! Term 1 was spent getting to know each other, building positive relationships and establishing high expectations. We delved into our learning and became Super Scientists and participated in many fun and exciting experiments. 
+  <span class="break"></span>
+  This year, we were lucky enough to experience exciting adventures and events that will provide many happy memories for years to come. Some of the highlights include the Cross Country, Swimming Lessons, Disco, Sport Fun Day, Under 8’s Day, excursions to Currumbin Wildlife Sanctuary and the Beenleigh Historical Village and Museum, as well as many, many dress up days. 
+  <span class="break"></span>
+  Special memories have been created in 2022. We have laughed together and worked through many challenges this year. 2A have been an absolute joy to teach and I am so proud of how far they have come. I wish the students all the best for 2023. 
+  <span class="break"></span>
+  2A – 2Awesome!`
+}
---
+++ public/classes/2B/data.js
@@ -0,0 +1,72 @@
+module.exports = {
+  title: '2B',
+  teachers: [
+      {name: 'Lisa Barnes', image: '/staff/Barnes, Lisa.jpg'},
+  ],
+  poster: '/classes/2B/poster.png',
+  pageNumbers: [40, 41],
+  colours: 'yr2',
+  classPhotos: [
+    {image: '/classes/2B/images/1.jpg', caption:  'Beau and Ellie making mixtures.'},
+    {image: '/classes/2B/images/2.jpg', caption:  'Candy, David and Jayden enjoying Sports Day.', left: '-18mm'},
+    {image: '/classes/2B/images/3.jpg', caption:  'Carter and Orion getting their science on.'},
+    {image: '/classes/2B/images/4.jpg', caption:  'Chantelle role playing in readers theatre.', top: '-6mm'},
+    {image: '/classes/2B/images/5.jpg', caption:  'Cooper making 3D happen.'},
+    {image: '/classes/2B/images/6.jpg', caption:  'Ellie, Tiaki and Xander rising to the Mathletics challenge.'},
+    {image: '/classes/2B/images/7.jpg', caption:  'Indi loving lolly mixtures.', left: '-23mm'},
+    {image: '/classes/2B/images/11.jpg', caption: 'Lexie and Nyjah making water work.'},
+    {image: '/classes/2B/images/13.jpg', caption: 'Mataio, Raiden and Charlie being safe, respectful learners.', top: '-14mm'},
+    {image: '/classes/2B/images/10.jpg', caption: 'Leityn and Mathew, go Foxwell.'},
+    {image: '/classes/2B/images/8.jpg', caption:  'Kahealani and Lachlan mixing it up.'},
+    {image: '/classes/2B/images/12.jpg', caption: 'Mathew and Nyjah, the shop is open.'},
+    {image: '/classes/2B/images/9.jpg', caption:  'Lachlan and Candy in traditional dress.'},
+    {image: '/classes/2B/images/14.jpg', caption: 'Rachelle making 3D delicious.'},
+    {image: '/classes/2B/images/15.jpg', caption: 'Uni and Yegana going back in time.'},
+    {image: '/classes/2B/images/16.jpg', caption: 'Tiaki and Tyles making a rainbow.'},
+  ],
+  tableTextStyle: {
+    fontSize: '7pt',
+    lineHeight: '10pt'
+  },
+  question: 'When I grow up I want to be…',
+  tableText: [
+    {name: 'Indiana	Bates', job: 'YouTuber'},
+    {name: 'Orion	Benson', job: 'Chef'},
+    {name: 'Xander	Bolin', job: 'YouTuber'},
+    {name: 'Mataio	Colombus', job: 'YouTuber'},
+    {name: 'Cooper	Dubbin', job: 'The President'},
+    {name: 'Nyjah	Gurr', job: 'YouTuber'},
+    {name: 'Mathew 	Heeb-Waters', job: 'Gardener'},
+    {name: 'Lexie	Kerr', job: 'Candy lady'},
+    {name: 'Uni 	Kim', job: 'Doctor in a hospital'},
+    {name: 'Kahealani	Kopua', job: 'Candy lady'},
+    {name: 'Leityn	Lurch', job: 'Nurse at a hospital'},
+    {name: 'Lachlan	Ma', job: 'Artist'},
+    {name: 'Tiaki	Manu', job: 'Rugby Union Player'},
+    {name: 'Tyles	Pahimbung', job: 'Gamer'},
+    {name: 'Charlie	Parangi', job: 'Electrician'},
+    {name: 'Beau	Reynolds', job: 'YouTuber'},
+    {name: 'Ellie	Richmond', job: 'Swimming Coach'},
+    {name: 'Rachelle	Sangil-Hudson', job: 'Tattoo Artist'},
+    {name: 'Jayden	Sarana', job: 'Boxer'},
+    {name: 'Yegana	Shivaee Garryz', job: 'Vet'},
+    {name: 'David	Sim', job: 'Gamer'},
+    {name: 'Chantelle	Sorrenson', job: 'Singer'},
+    {name: 'Carter	Tibaldi', job: 'YouTuber'},
+    {name: 'Raiden	Vance-Smith', job: 'Taxi Driver'},
+    {name: 'Candy 	Zhang', job: 'Youtuber'},
+  ],
+  textStyle: {
+    fontSize: '8.5pt',
+    lineHeight: '11pt',
+  },
+  text: `I am so incredibly grateful to have had the twenty-five students that made up the 2B class of 2022.
+  <span class="break"></span>
+  Thanks to everyone, students' parents and carers, for being there and offering your support through the year, you’re the best! I will miss seeing you all in 2B every day? 
+  <span class="break"></span>
+  It’s been a pleasure to watch each student grow and change this year. You have within you, everything you need to continue to be successful. You can move mountains if you want. I wish all of my students, much success in the future.
+  <span class="break"></span>
+  I know next year is going to be amazing! I hope that you all have a great Summer break and that you remember 2022 in Year 2.
+  <span class="break"></span>
+  Miss Barnes.`
+}
