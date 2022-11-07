6e28dc784a3cef621a888bf24cc709ae3d35aa84
Joshua <joshua@grimmett.com.au>
2022-11-08 07:55:51 +1000

    Coaching

---
+++ pages/coaching/index.tsx
@@ -0,0 +1,70 @@
+import type { NextPage } from "next"
+import { Photo } from "../../components/ClassPhotoCards"
+import { FixedFooter } from "../../components/Footer"
+import { PlainHeader } from "../../components/Header"
+
+import Layout from "../../components/Layout"
+import Tab from "../../components/Tab"
+
+import coaching from '../../public/other/coaching/data'
+
+const LeftPage = (props: any) => (
+  <div className="left">
+      <Tab/>
+
+      <div className="relative page__content">
+        <PlainHeader
+          textStyle={{fontSize: '30pt'}}
+          direction='left'
+          title={props.title}/>
+        
+        <div className="grid grid-cols-3 w-[179mm] gap-[5mm] top-[50mm] absolute">
+          <span className='text-justify col-span-3 columns-3' style={props.textStyle} dangerouslySetInnerHTML={{__html: props.text}}></span>
+          <div className="grid grid-cols-3 col-span-3 gap-[3mm] flex-wrap">
+            {props.images.slice(0,9).map((image: any, key: number) => (
+              <Photo photo={image} height="45.4mm" width="100%" key={key}/>
+            ))}
+          </div>
+        </div>
+
+        <FixedFooter direction='left' pageNumber={props.pageNumber}/>
+      </div>
+  </div>
+)
+
+const RightPage = (props: any) => (
+  <div className="right">
+      <Tab/>
+      <div className="relative page__content">
+        <PlainHeader 
+          textStyle={{fontSize: '30pt'}}
+          direction='right'
+          title={props.title}/>
+        
+        <div className="grid w-[179mm] gap-[5mm] top-[50mm] absolute">
+          <div className="relative">
+            <div className="grid grid-cols-2 gap-[3mm]">
+              {props.images.slice(9).map((image: any, key: number) => (
+                <Photo photo={image} height="52.3mm" width="100%" key={key}/>
+              ))}
+            </div>
+          </div>
+        </div>
+        
+        <FixedFooter direction='right' pageNumber={props.pageNumber}/>
+      </div>
+  </div>
+)
+
+const CouncilTemplate: NextPage = () => {
+  return (
+    <Layout title={coaching.title}>
+      <div className={`page ${coaching.colours}`}>
+        <LeftPage {...coaching}/>
+        <RightPage {...coaching}/>
+      </div>
+    </Layout>
+  )
+}
+
+export default CouncilTemplate
---
+++ public/other/coaching/data.js
@@ -0,0 +1,42 @@
+module.exports = {
+  colours: 'plain',
+  title: "Coaching",
+  textStyle: {
+    fontSize: "9.5pt",
+    lineHeight: "11pt",
+  },
+  text: `The CRSS Coaching Team experienced another exciting year as we enjoyed working alongside an amazing teaching team consisting of Teachers and Assistant Teachers.
+  <span class="break"></span>
+  A vital aspect of our Professional Engagement Framework is coaching. In 2022, our focus has been on creating an expert teaching team in reading and this will continue into 2023. We have also been able to support teachers with innovative teaching strategies across all learning areas.
+  <span class="break"></span>
+  <h1>Assistant Teachers</h1>
+  <span class="break"></span>
+  This year our ATs have engaged in regular professional development across two fundamental areas; supporting students who are learning to read and supporting collaboration skills. We are fortunate to have a dedicated team of Assistant Teachers who are consistently open to learning and practicing their skills.
+  <span class="break"></span>
+  <h1>Teachers</h1>
+  <span class="break"></span>
+  In the early years of Prep to Year 2, the focus has been around the teaching of phonics for our students who are learning to read. We have been fortunate to have support from external providers who are experts in this area. This expertise has assisted us in developing expert teachers of reading. The newly formed ‘Reading Collective’ will continue to drive innovation and development around reading.
+  <span class="break"></span>
+  Across the school, work has continued from previous years relating to the CRSS Pedagogical Framework – Deep Learning. All staff have engaged in fortnightly Professional Learning Team meetings to create clarity around our whole school Good at Learning: Good at Life strategies. 
+  Our Deep Learning framework has developed throughout the year with teaching staff acquiring new skills and strategies to encourage students to work collaboratively.  `,
+  images: [
+    {image: "/other/coaching/1.jpeg"},
+    {image: "/other/coaching/2.jpeg"},
+    {image: "/other/coaching/3.jpeg"},
+    {image: "/other/coaching/4.jpeg"},
+    {image: "/other/coaching/5.jpeg"},
+    {image: "/other/coaching/6.jpeg"},
+    {image: "/other/coaching/7.jpeg"},
+    {image: "/other/coaching/8.jpeg"},
+    {image: "/other/coaching/9.jpeg"},
+    {image: "/other/coaching/10.jpeg", top: '-63mm'},
+    {image: "/other/coaching/11.jpeg", top: '-25mm'},
+    {image: "/other/coaching/12.jpeg"},
+    {image: "/other/coaching/13.jpeg", top: '-13mm'},
+    {image: "/other/coaching/14.jpeg"},
+    {image: "/other/coaching/15.jpeg"},
+    {image: "/other/coaching/16.jpeg", top: '-40mm'},
+    {image: "/other/coaching/17.jpeg", top: '-5mm'},
+  ],
+  pageNumber: 1,
+}
