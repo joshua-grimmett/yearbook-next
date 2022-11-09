bca8044c9d943c3e62d595dc1cbf03006f001917
Joshua <joshua@grimmett.com.au>
2022-11-09 17:38:28 +1000

    Principal

---
+++ pages/principal_p&c/index.tsx
@@ -0,0 +1,95 @@
+import type { NextPage } from "next"
+import { Photo } from "../../components/ClassPhotoCards"
+import DeputyQuote from "../../components/DeputyQuote"
+import { FixedFooter } from "../../components/Footer"
+import { PlainHeader } from "../../components/Header"
+
+import Layout from "../../components/Layout"
+import PlainStaffCard from "../../components/PlainStaffCard"
+
+import data from '../../public/other/principal/data'
+import Tab from "../../components/Tab"
+
+const LeftPage = (props: any) => (
+  <div className="left">
+      <Tab/>
+      <div className="relative page__content">
+        <PlainHeader textStyle={{
+          fontSize: '21pt'
+        }} imageInHeader={true} direction='left' title={props.title}/>
+        
+        <div className="topGrid">
+          <DeputyQuote text={props.quote} textStyle={{
+            top: '36.7mm'
+          }}/>
+          <div className="right top-[-11mm]">
+            <PlainStaffCard staff={props.staff}/>
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-2 w-[179mm] gap-[5mm] top-[82mm] absolute">
+          <span className='text-justify longBreak' style={props.textStyle} dangerouslySetInnerHTML={{__html: props.text[0]}}></span>
+          <div className="relative">
+            <div className="grid gap-[3mm]">
+            <span className='text-justify longBreak' style={props.textStyle} dangerouslySetInnerHTML={{__html: props.text[1]}}></span>
+              <Photo photo={props.images[0]} width="100mm" height="39.6mm"/>
+              <Photo photo={props.images[1]} width="100mm" height="39.6mm"/>
+              <Photo photo={props.images[2]} width="100mm" height="39.6mm"/>
+            </div>
+          </div>
+        </div>
+
+
+        <FixedFooter direction='left' pageNumber={props.pageNumber}/>
+      </div>
+  </div>
+)
+
+const RightPage = (props: any) => (
+  <div className="right">
+      <Tab/>
+      <div className="relative page__content">
+        <PlainHeader textStyle={{
+          fontSize: '21pt'
+        }} imageInHeader={true} direction='right' title={props.title}/>
+        
+        <div className="grid w-[179mm] gap-[5mm] top-[50mm] absolute">
+          <span className='text-justify longBreak columns-3' style={props.textStyle} dangerouslySetInnerHTML={{__html: props.text}}></span>
+          <div className="relative">
+            <div className="flex flex-row gap-[3mm] mt-[3mm] relative max-w-[179mm]">
+              <Photo photo={props.images[8]} width="200mm" height="43.6mm"/>
+              <Photo photo={props.images[0]} width="100mm" height="43.6mm"/>
+            </div>
+            <div className="grid grid-cols-3 gap-[3mm] mt-[3mm] relative">
+              <div className="flex flex-col gap-[3mm]">
+                <Photo photo={props.images[3]} width="100mm" height="43.6mm"/>
+                <Photo photo={props.images[5]} width="100mm" height="43.6mm"/>
+                <Photo photo={props.images[6]} width="100mm" height="43.6mm"/>
+              </div>
+              <div className="flex flex-col gap-[3mm]">
+                <Photo photo={props.images[4]} width="100mm" height="43.6mm"/>
+                <Photo photo={props.images[7]} width="100mm" height="43.6mm"/>
+              </div>
+              <div className="flex flex-col gap-[3mm]">
+                <Photo photo={props.images[1]} width="100mm" height="43.6mm"/>
+                <Photo photo={props.images[2]} width="100mm" height="43.6mm"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <FixedFooter direction='right' pageNumber={props.pageNumber}/>
+      </div>
+  </div>
+)
+
+const CouncilTemplate: NextPage = () => {
+  return (
+    <Layout title={data.title}>
+      <div className={`page ${data.colours}`}>
+        <LeftPage {...data}/>
+      </div>
+    </Layout>
+  )
+}
+
+export default CouncilTemplate
---
+++ public/other/principal/data.js
@@ -0,0 +1,46 @@
+module.exports = {
+  colours: 'plain',
+  title: "Principal’s Message",
+  textStyle: {
+    fontSize: "8.5pt",
+    lineHeight: "10pt",
+  },
+  quote: `“Piglet noticed that even though he had a Very Small Heart, it could hold a rather lot of Gratitude.” <br/>AA Milne, (Winnie-the-Pooh)`,
+  text: [`Dear mums, dads, carers, students and staff
+  <span class="break"></span>
+  The 2022 school year has drawn to a close and, once again, we can look back on our achievements with pride and satisfaction.  
+  <span class="break"></span>
+  My personal headline events and achievements this year (in no particular order) include:
+  <li>Kokoda challenge</li>
+  <li>Wakakirri (and we made the finals)</li>
+  <li>The vast range of extra-curricular opportunities in the face of teacher shortages</li>
+  <li>Deep learning in our classrooms</li>
+  <li>Receiving Apple Distinguished Schools recognition … again</li>
+  <li>A deeper focus on sustainability across the school.</li> 
+  <span class="break"></span>
+  There is a distinct difference between offering a service and being willing to serve others.  They both include giving, but only one is generous and selfless.  It is this willingness or goodwill that makes Coomera Rivers that little bit different.  The strength of any school can be measured by its goodwill – that intangible amount that is freely contributed to the students, families, and school reputation by those who are willing to serve.  It is visible each time parents participate with their children at a school event, when teachers organise activities that are outside their classroom duties or P&C executive conduct an activity for our school community.  
+  <span class="break"></span>
+  Both good and great schools will have strong academic performances.  But great schools have people who selflessly create goodwill.  They display those human traits of compassion, care and kindness that make a difference for others.  This is something that our school is teaching students about as well.  
+  <span class="break"></span>
+  Thank you to our families and volunteers who have contributed to our successes and activities this past year.  You have played a valuable role in the lives of our children, and I am extremely grateful to you.  
+  <span class="break"></span>
+  Thank you to our school staff who have all contributed to the success of 2022.  Their contributions outside the classroom have been enormous and our children have benefitted greatly from their expertise and commitment.  I am deeply satisfied and incredibly proud of their work.  
+  <span class="break"></span>
+  Thank you to our P&C who have worked tirelessly again this year.  These are wonderful volunteers who operate our OSHC and invest in our facilities and sense of community.  Everyone will have seen the additional facilities that the P&C has contributed but it is the extra opportunities like sports uniforms, Year 6 shirts, this yearbook that make our P&C sensational.  
+  <span class="break"></span>
+  `,`I would like to wish the graduating students of 2022 well as they transition to their various secondary schools.  I am grateful to them for their character and leadership, and, along with those families who are leaving Coomera Rivers at the end of this year, I hope they can look back on their primary school with fondness and pride.
+  <span class="break"></span>
+  With the opening of three primary schools in our suburb across the past five years or so, our school enrolments will continue to slowly decline.  This will soon become visible with changes in our staff.  I anticipate that both Mrs Artz and I will be transferred to other Gold Coast schools by the time that this year book is published.  I am sure that I speak for both of us when I say that our time at Coomera Rivers will always be a special part of our careers.  
+  <span class="break"></span>
+  Kind regards
+  <br>Vic`],
+  images: [
+    {image: "/other/principal/1.png"},
+    {image: "/other/principal/3.png"},
+    {image: "/other/principal/2.png"},
+  ],
+  pageNumber: 1,
+  staff: [
+    {name: "Vic Graham", image: "/staff/Graham, Vic.jpg"},
+  ]
+}
